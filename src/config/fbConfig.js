86337a45c010ff21bdd7c474c7c95346087b2d25
amolkk.github.io <amolkakkar95@gmail.com>
2020-05-15 21:19:11 -0700

    first commit react projectM

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+  apiKey: "AIzaSyBV4aMfu9XizfICXxwqaVgXCS0SFv831n0",
+  authDomain: "projectm-2aba4.firebaseapp.com",
+  databaseURL: "https://projectm-2aba4.firebaseio.com",
+  projectId: "projectm-2aba4",
+  storageBucket: "projectm-2aba4.appspot.com",
+  messagingSenderId: "599179987900",
+  appId: "1:599179987900:web:d8184f536d030c85ef39b9",
+  measurementId: "G-QWZ6QK3KKJ"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
